a06e3e9cc30aa4f751b429044687a32e0efd307b
Alie Stone <helloaliestone@gmail.com>
2020-09-09 17:02:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(numbers)
     numbers =[1,2,3,4]
     new_array = []
-    numbers.each {|n| puts n**2}
+    square_array.each {|n| puts n**2}
     new_array << numbers
     return new_array
 end
 
-#return value of calling each on an array 
+#return value of calling each on an array
