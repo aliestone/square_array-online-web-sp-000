6b13f65396c61b2101ec97a6c624bd79d8cc7da4
Alie Stone <helloaliestone@gmail.com>
2020-09-09 17:01:23 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,6 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+    new_array = []
+    square_array.each { |e| e ** 2 }
+    new_array << square_array
+    return new_array
 end
